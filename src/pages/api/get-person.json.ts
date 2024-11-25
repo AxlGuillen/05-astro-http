bc3f334a76d84e189841413576334e4b7dcf134a
Axl Eduardo Guillen Luna <axl1504@hotmail.com>
2024-11-24 19:50:42 -0600

    Primer endpoint

---
+++ src/pages/api/get-person.json.ts
@@ -0,0 +1,13 @@
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async ({ params, request }) => {
+    const person = {
+        name: 'Axl',
+        age: 22,
+    }
+
+    return new Response(JSON.stringify(person), { 
+        status: 200, 
+        headers: { 'Content-Type': 'application/json' }
+    } )
+}
