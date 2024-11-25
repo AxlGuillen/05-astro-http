7bf7f74adaa8529902bb62efebf8c8aa38109b8e
Axl Eduardo Guillen Luna <axl1504@hotmail.com>
2024-11-24 20:42:48 -0600

    add API routes for blog posts with GET, POST, PUT, PATCH, and DELETE methods

--- src/pages/api/posts/index.ts
+++ src/pages/api/posts/index.ts
@@ -1,8 +1,28 @@
 import type { APIRoute } from "astro";
 import { getCollection } from "astro:content";
 
+export const prerender = false;
+
 export const GET: APIRoute = async ({ params, request }) => {
   const blogs = await getCollection("blog");
+
+  const url = new URL(request.url);
+  const slug = url.searchParams.get('slug');
+
+  if (slug) {
+    const post = blogs.find((blog) => blog.slug === slug);
+
+    if (post) {
+        return new Response(JSON.stringify(post), {
+            status: 200,
+            headers: { "Content-Type": "application/json" },
+        });
+    } else
+        return new Response(JSON.stringify({ message: "Not found" }), {
+            status: 404,
+            headers: { "Content-Type": "application/json" },
+        });
+  }
 
   return new Response(JSON.stringify(blogs), {
     status: 200,
